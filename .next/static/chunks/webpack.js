57f92d73f20c5a1ffc86ac8d04b12f051f7a144e
Lucas Lira <lucasrocha446@gmail.com>
2020-10-12 00:08:34 -0300

    Client Side Fetching

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "af8f0f5df9ca3eed62ea";
+/******/ 	var hotCurrentHash = "6d8689120709d6a79de9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,37 @@
+import React, { useState, useEffect } from 'react';
 import { Title } from '../styles/Pages/Home';
 
+interface IProject {
+  id: string,
+  title: string
+}
+
 export default function Home() {
+  const [recommendedProducts, setRecommendedProducts] = useState<IProject>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/recommended').then(response => {
+      response.json().then(data => {
+        setRecommendedProducts(data);
+      });
+    });
+  }, []);
+
   return (
-    <Title>Hello World!</Title>
+    <div>
+      <section>
+        <Title>Products</Title>
+
+        <ul>
+          {recommendedProducts.map(recommendedProduct => {
+            return (
+              <li key={recommendedProduct.id}>
+                {recommendedProduct.title}
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    </div>
   )
 }
